8904dcb02fd1891aed12404ea37681d2b51e2859
Ahnjihyeon <vhxjwkd12@gmail.com>
2020-12-14 17:46:06 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AppRouter from './component/route/RouterComponent';
+import NavBar from './component/route/NavBar';
+import Container from '@material-ui/core/Container'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  return(
+    <div>
+      <NavBar />
+      <Container>
+        <AppRouter />
+      </Container>
     </div>
   );
 }
